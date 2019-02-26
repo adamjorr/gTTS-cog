0e14525c5f6edabc4ba7187036a26d317c872957
Adam Orr <adamjorr@gmail.com>
2019-02-26 02:13:15 -0700

    just use a tmp file

--- gtts/gtts.py
+++ gtts/gtts.py
@@ -15,7 +15,7 @@ class Gtts(commands.Cog):
         """Have the bot say something."""
         tts = gTTS(query, lang)
         audiopath = cog_data_path(raw_name='Audio')
-        with tempfile.NamedTemporaryFile(dir=str(audiopath / localtracks) + '/', suffix = '.mp3') as tmpfile:
+        with tempfile.NamedTemporaryFile(dir=str(audiopath / 'localtracks') + '/', suffix = '.mp3') as tmpfile:
             playfp = pathlib.Path(tmpfile.name).relative_to(audiopath)
             tts.write_to_fp(tmpfile)
             await ctx.invoke(self.play, query = 'localtracks:{}'.format(str(playfp)))
