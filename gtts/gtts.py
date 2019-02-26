61f43f72f9afb10120afa926f105775b1d5e5f43
Adam Orr <adamjorr@gmail.com>
2019-02-26 01:10:04 -0700

    use temp file and pipe instead

--- gtts/gtts.py
+++ gtts/gtts.py
@@ -1,9 +1,9 @@
 from redbot.core import commands
-import lavalink
 from gtts import gTTS
-from io import BytesIO
-import codecs
 from urllib.parse import quote
+import os
+from redbot.core.data_manager import cog_data_path
+import tempfile
 
 class Gtts(commands.Cog):
     """Speak using gTTS."""
@@ -11,26 +11,11 @@ class Gtts(commands.Cog):
     @commands.command()
     async def say(self, ctx, *, query, lang = 'en'):
         """Have the bot say something."""
-        bytefp = BytesIO()
         tts = gTTS(query, lang)
-        tts.write_to_fp(bytefp)
-        bytestr = codecs.encode(bytefp.getvalue(), encoding = 'base64').decode()
-        bytestr = bytestr.replace('+','-')
-        bytestr = bytestr.replace('/','_')
-        player = lavalink.get_player(ctx.guild.id)
-        req_url = "http://{}:{}/decodetrack?track={}".format(
-            player._node.host, player._node.rest, bytestr)
-        async with player._session.get(req_url, headers = player._headers) as resp:
-            data = await resp.json(content_type=None)
-        try:
-            track = data["tracks"] if type(data) is dict else data
-        except KeyError:
-            print("request:")
-            print(req_url)
-            print("data:")
-            print(data)
-            raise
-        track = Track(track[0])
-        player.add(ctx.author, track)
-        if not player.current:
-            player.play
+        audiopath = cog_data_path(raw_name='Audio')
+        async with tempfile.TemporaryDirectory(prefix=audiopath) as tmpdir:
+            pipefp = pathlib.Path(tmpdir) / 'gtts.mp3'
+            os.mkfifo(pipefp)
+            tts.save(pipefp)
+            playfp = pipefp.relative_to(audiopath)
+            await ctx.invoke(self.play, query = 'localtracks:{}'.format(str(playfp)))
