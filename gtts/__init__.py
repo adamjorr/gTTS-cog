fe22583d885be0318ae9839bcee9a1d55dd2575f
Adam Orr <adamjorr@gmail.com>
2019-02-25 21:51:48 -0700

    initial commit

---
+++ gtts/__init__.py
@@ -0,0 +1,4 @@
+from .gtts import Gtts
+
+def setup(bot):
+	bot.add_cog(Gtts())
---
+++ gtts/gtts.py
@@ -0,0 +1,25 @@
+from redbot.core import commands
+import lavalink
+from gtts import gTTS
+from io import BytesIO
+
+class Gtts(commands.Cog):
+    """Speak using gTTS."""
+
+    @commands.command()
+    async def say(self, ctx, *, query, lang = 'en'):
+        """Have the bot say something."""
+        bytefp = BytesIO()
+        tts = gTTS(query, lang)
+        tts.write_to_fp(bytefp)
+        bytestr = bytefp.getvalue()
+        player = lavalink.get_player(ctx.guild.id)
+        req_url = "http://{}:{}/decodetrack?track={}".format(
+            player._node.host, player._node.rest, bytestr)
+        async with player._session.get(req_url, headers = player._headers) as resp:
+            data = await resp.json(content_type=None)
+        track = data["tracks"] if type(data) is dict else data
+        track = Track(track[0])
+        player.add(ctx.author, track)
+        if not player.current:
+            player.play
